e275e65ba6e404f396773ed8c2b702d03d85b86a
Sergio Ribera <sergioalejandroriberacosta@gmail.com>
2022-12-13 13:05:15 -0400

    feat: move principal algorithm to library and update imports

--- src/lib.rs
+++ src/lib.rs
@@ -9,3 +9,40 @@ pub mod prelude {
     pub use super::lang::*;
     pub use super::user_input::*;
 }
+
+// **********************************
+// *                                *
+// *      The Tree Algorithm        *
+// *                                *
+// **********************************
+// private function
+pub fn generate_tree(n: i32, padding: i32, sentence: &[&'static str]) -> Result<(), &'static str> {
+    // if n is 0, throw error
+    if n <= 0 {
+        // & is a reference
+        // * use for dereference
+        // the first * is for dereference &[] array
+        // the second * is for derefence &'static str
+        // and the & on first is for made a new reference to &'static str
+        //
+        // It for fix the clippy borrow-deref-ref warning
+        return Err(&**sentence.get(3).unwrap());
+    }
+    // Run from 0 to n, for made n lines
+    for i in 0..n {
+        // calculate space from left
+        let n_spaces = (n - i - 1) + padding;
+        // calculate stars to draw
+        let n_stars = (i * 2) + 1;
+        // generate spaces string
+        // first create a vector with n_spaces length filled by " " space character
+        let spaces = vec![" "; n_spaces as usize].join("");
+        // generate star string
+        // first create a vector with n_stars length filled by "*" star character
+        let stars = vec!["*"; n_stars as usize].join("");
+
+        // show line on console
+        println!("{spaces}{stars}")
+    }
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,10 @@
-use lang::{is_language_valid, LANGUAGES};
-use user_input::{accept, ask, is_n, is_number, MyBool};
-
 // import library for print with colors
 use colored::Colorize;
-
-// define the modules to rust compiler
-mod lang;
-mod user_input;
-
-// **********************************
-// *                                *
-// *      The Tree Algorithm        *
-// *                                *
-// **********************************
-// private function
-fn generate_tree(n: i32, padding: i32, sentence: &[&'static str]) -> Result<(), &'static str> {
-    // if n is 0, throw error
-    if n <= 0 {
-        // & is a reference
-        // * use for dereference
-        // the first * is for dereference &[] array
-        // the second * is for derefence &'static str
-        // and the & on first is for made a new reference to &'static str
-        //
-        // It for fix the clippy borrow-deref-ref warning
-        return Err(&**sentence.get(3).unwrap());
-    }
-    // Run from 0 to n, for made n lines
-    for i in 0..n {
-        // calculate space from left
-        let n_spaces = (n - i - 1) + padding;
-        // calculate stars to draw
-        let n_stars = (i * 2) + 1;
-        // generate spaces string
-        // first create a vector with n_spaces length filled by " " space character
-        let spaces = vec![" "; n_spaces as usize].join("");
-        // generate star string
-        // first create a vector with n_stars length filled by "*" star character
-        let stars = vec!["*"; n_stars as usize].join("");
-
-        // show line on console
-        println!("{spaces}{stars}")
-    }
-    Ok(())
-}
+use rust_triangle_star::{
+    generate_tree,
+    prelude::{is_language_valid, LANGUAGES},
+    user_input::{accept, ask, is_n, is_number, MyBool},
+};
 
 fn main() -> Result<(), &'static str> {
     let mut cont = true;
